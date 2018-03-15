6dedf9486de93622ff498ad4fbc397da7976f60f
Topi Sikanen <topi.sikanen@gmail.com>
2018-03-15 12:56:28 +0200

    Add setup.py script for proper packaging

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyropython',
+      version='0.01',
+      description='Python utility for pyrolysis parameter identification',
+      url='https://ringworld.vtt.fi/scm/git/PyroPython',
+      author='Topi Sikanen',
+      author_email='topi.sikanen@vtt.fi',
+      license='VTT',
+      packages=['pyropython'],
+      zip_safe=False)
