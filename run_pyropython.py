7338845cc94053b43c50a9b34af69aed91772ee8
Sikanen Topi <Topi.Sikanen@vtt.fi>
2018-03-22 14:04:56 +0200

    Add python runner script

---
+++ run_pyropython.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 20 09:59:59 2018
+
+@author: tstopi
+"""
+
+import sys
+from pyropython.pyropython import main
+from multiprocessing import freeze_support
+
+
+if __name__ == "__main__":
+    freeze_support()
+    main()
