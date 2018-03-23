5dbf6d771362314071284aa41d87503a1e5eb7cf
Topi Sikanen <topi.sikanen@gmail.com>
2018-03-23 16:44:10 +0200

    Add comments to source

--- pyropython/objective_functions.py
+++ pyropython/objective_functions.py
@@ -9,18 +9,30 @@ from numpy import mean,abs,var
 import sys
 
 
-
 def standardized_moment(edata,sdata,weights,p=2):
     """
     Measure based on standardized moment: 
-        E[(sum_i ( w_i *(y-y_hat) )^p]/std(weights*y)^p
+        E[(sum_i ( w_i *(y_i-yhat_i) )^p]/std(w*y)^p
+    where:
+       edata  : y_i   , experimental data
+       sdata  : yhat_i, simulation   
+       weights: w_i   , data weights 
+    All inputs are assumed to be one dimensional arrays 
+    of equal lengths.
+
+    Returns:
+        the value of the standardized moment
     """
+
     dev = (weights*abs(edata-sdata))**p
     return mean(dev)/var(weights*edata)**(p/2.0)
 
 objective_functions={"standardized_moment": standardized_moment}
 
 def get_objective_function(name="standardized_moment"):
+    """
+    Returns the objective function with the given name
+    """
     if name in objective_functions:
         func = objective_functions[name]
     else:
